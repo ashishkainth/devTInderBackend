b36f9e210fa1d8be6fbb8472e1a45ea61c91cdc1
kainthashish <kainth.ashish@gmail.com>
2024-09-19 13:44:16 +0530

    Get post

--- src/app.js
+++ src/app.js
@@ -2,17 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello From Server");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Ashish", lastName: "Kainth" });
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello Test From Server");
+app.post("/user", (req, res) => {
+  res.send({ responseCode: 200, message: "Success" });
 });
 
-app.use("/", (req, res) => {
-  res.send("Hello Home Home From Server");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello Hello From Server");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("Hello Test From Server");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Hello Home Home From Server");
+// });
 
 app.listen(3000, () => {
   console.log("Server Started");
