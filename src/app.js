d3ea9813a288598d3759580a5eba61776d96e564
kainthashish <kainth.ashish@gmail.com>
2024-09-20 13:14:29 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -4,19 +4,28 @@ const app = express();
 
 const { adminAuth } = require("./middleware/auth");
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllUser", (req, res, next) => {
-  res.send("All Data");
+app.get("/getAllData", (req, res, next) => {
+  throw new Error("Random Error!");
+  res.send("All data!");
 });
 
-app.post("/admin/addUser", (req, res, next) => {
-  res.send("User addedd Successfully");
+app.use("/", (err, req, res, next) => {
+  if (err) res.status(500).send("Something went wrong!");
 });
 
-app.delete("/admin/deleteUser", (req, res, next) => {
-  res.send("User Deleted!");
-});
+// app.use("/admin", adminAuth);
+
+// app.get("/admin/getAllUser", (req, res, next) => {
+//   res.send("All Data");
+// });
+
+// app.post("/admin/addUser", (req, res, next) => {
+//   res.send("User addedd Successfully");
+// });
+
+// app.delete("/admin/deleteUser", (req, res, next) => {
+//   res.send("User Deleted!");
+// });
 
 app.listen(3000, () => {
   console.log("Server Started");
